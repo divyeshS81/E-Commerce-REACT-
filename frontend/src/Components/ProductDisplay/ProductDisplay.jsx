367d9091b3fa05f7392a70016205823db4c4c3a1
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-06 12:36:23 +0530

    add productdisplay section

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -28,6 +28,26 @@ export const ProductDisplay = (props) => {
               <img src={star_dull_icon} alt="" />
               <p>(122)</p>
             </div>
+            <div className="productdisplay-right-prices">
+              <div className="productdisplay-right-price-old">${product.old_price}</div>
+              <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+              A lightweight, usuall knitted pullover shirt, close-fitting and
+            </div>
+            <div className="productdisplay-right-size">
+              <h1>Select Size</h1>
+              <div className="productdisplay-right-size">
+                  <div>S</div> 
+                  <div>M</div>
+                  <div>L</div> 
+                  <div>XL</div>
+                  <div>XXl</div>              
+              </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women , T-shirt, Crop Top</p>
+             <p className='productdisplay-right-category'><span>Tages :</span>Mordrn,latest</p>
         </div>
     </div>
   )
